d7da93c5f88927f67db806aab3291c88f64d36c2
Hydria <liverpol2222@gmail.com>
2023-02-21 13:42:56 +0000

    Fixed Exception + LN adjustment snapshot
    turns out if there were no notes that had different start times, the program would throw an exception
    on top of this, i've worked on realigning all of the LN maps so that harder ones get a bit of a buff (however there is still 1 or 2 maps causing issues rn) so work continues

--- osu.Game.Rulesets.Mania/Difficulty/Skills/Strain.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Skills/Strain.cs
@@ -6,6 +6,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using osu.Framework.Input.Events;
 using osu.Framework.Utils;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Difficulty.Skills;
@@ -49,8 +50,9 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
 
             // --- OKAY LETS START BY SOLVING THE CHORD / JUMPTRILL ISSUE --- //
             //find the last note used
+            double lastNote = 0;
             double[] lastNoteList = startTimes.Where(c => c != startTime).ToArray();
-            double lastNote = lastNoteList.Max();
+            if (lastNoteList.Length != 0) { lastNote = lastNoteList.Max(); }
 
             //see what columns use that note (chord checker)
             int[] columnTracker = new int[startTimes.Length];
@@ -184,29 +186,30 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
             if (handBias) { handBiasValue *= 0.4; }
 
             //time to try another calculation aka how far through the LN are we hitting, the further from the middle of the note we're hitting the easier it is.
-            double lnProgressionPercentage = 0;
+            double lnProgressionPercentage = 1;
             for (int i = 0; i < endTimes.Length; ++i)
             {
-                if (Precision.DefinitelyBigger(endTimes[i], startTime, 1) && Precision.DefinitelyBigger(endTime, endTimes[i], 1))
+                if (Precision.DefinitelyBigger(startTime, startTimes[i], 1) && Precision.DefinitelyBigger(endTime, endTimes[i], 1))
                 {
-                    if (lnProgressionPercentage == 0) { lnProgressionPercentage = (endTimes[i] - startTime) / (endTime - startTime); }
-                    else { lnProgressionPercentage = (lnProgressionPercentage + (endTimes[i] - startTime) / (endTime - startTime)) / 2; }
+                    if (lnProgressionPercentage == 0) { lnProgressionPercentage = ((endTimes[i] - startTime) / (endTime - startTime)) * (1 / (1 + Math.Exp(0.5 * ((release_threshold * 2) - (endTimes[i] - startTimes[i]))))); }
+                    else { lnProgressionPercentage = (lnProgressionPercentage + (((endTimes[i] - startTime) / (endTime - startTime))) * (1 / (1 + Math.Exp(0.5 * ((release_threshold * 2) - (endTimes[i] - startTimes[i])))))) / 2; }
                 }
             }
-            if (lnProgressionPercentage <= 0.5) { lnProgressionPercentage *= 2; }
-            else { lnProgressionPercentage = 2 - (lnProgressionPercentage * 2); }
+            if (lnProgressionPercentage <= 0.5) { lnProgressionPercentage *= 4; }
+            else { lnProgressionPercentage = 4 - (lnProgressionPercentage * 4); }
+
             // The hold addition is given if there was an overlap, however it is only valid if there are no other note with a similar ending.
             // Releasing multiple notes is just as easy as releasing 1. Nerfs the hold addition by half if the closest release is release_threshold away.
             // holdAddition
             //      ^
-            // 1.0  + - - - - - -+-----------
+            // 2.3  + - - - - - -+-----------
             //      |           /
-            // 0.65 + - - - - -/   Sigmoid Curve
+            // 1.15 + - - - - -/   Sigmoid Curve
             //      |         /|
-            // 0.3  +--------+-+---------------> Release Difference / ms
+            // 0.0  +--------+-+---------------> Release Difference / ms
             //          release_threshold
             if (isOverlapping)
-                holdAddition = (0.7 / (1 + Math.Exp(0.5 * (release_threshold - (closestEndTime * handBiasValue * lnProgressionPercentage))))) + 0.3;
+                holdAddition = 2.3 / (1 + Math.Exp(0.5 * (release_threshold - (closestEndTime * handBiasValue * lnProgressionPercentage))));
 
             // Decay and increase individualStrains in own column
             individualStrains[column] = applyDecay(individualStrains[column], startTime - startTimes[column], individual_decay_base, false);
