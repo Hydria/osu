e53efe70669ebafa114afc1ac1f91378d04ba88e
Hydria <liverpol2222@gmail.com>
2023-02-24 14:51:07 +0000

    added complexity values to calculation
    Seems like the general consensus was to get the complexity implementation in now to work with later.
    This uses the easier to calculate non-hand related formula as the difference between the two in terms of results was negligible (and this one produced better nerfs for overrated chords).

--- osu.Game.Rulesets.Mania/Difficulty/Preprocessing/ManiaDifficultyHitObject.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Preprocessing/ManiaDifficultyHitObject.cs
@@ -3,6 +3,7 @@
 
 #nullable disable
 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using osu.Game.Rulesets.Difficulty.Preprocessing;
@@ -21,22 +22,42 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Preprocessing
         public ManiaDifficultyHitObject(HitObject hitObject, HitObject lastObject, double clockRate, List<DifficultyHitObject> objects, int index)
             : base(hitObject, lastObject, clockRate, objects, index)
         {
-            this.objects = objects;
-            this.hitObject = hitObject;
         }
 
         /// <summary>
-        /// Returns the bool value of the current chord (true if there is a note in the column at that time).
+        /// Returns the complexity of the current chord based on its bit values.
         /// </summary>
         /// <param name="columns">The key amount.</param>
         /// <returns></returns>
-        public bool[] ChordValue(int columns)
+        public double ChordComplexity(int columns)
         {
-            bool[] currentChord = new bool[columns];
+            bool[] currentChord = new bool[columns + 2];
+            double chordComplexity = 0;
+            double specialColumn = 0;
+
             currentChord[((ManiaHitObject)hitObject).Column] = true;
             foreach (ManiaDifficultyHitObject d in objects.Where(o => o.StartTime == StartTime))
-                currentChord[d.BaseObject.Column] = true;
-            return currentChord;
+                currentChord[d.BaseObject.Column + 1] = true; //sets true if note is present in chord, places it one value down to allow a buffer to avoid issues with upscaling keymodes
+            if (columns % 2 != 0)
+            {
+                //need to remove the middle note as use that as a separate calculation
+                if (currentChord[Convert.ToInt32(Math.Ceiling(currentChord.Length / 2.0))])
+                {
+                    specialColumn = 3;
+                }
+                //rebuild array without special column
+                currentChord.Where((_, i) => i != Math.Ceiling(currentChord.Length / 2.0)).ToArray();
+            }
+
+            for (int i = 1; i < columns + 1; i++)
+            {
+                if (currentChord[i])
+                    chordComplexity += 1.0;
+                if ((currentChord[i] && !currentChord[i]) || (!currentChord[i] && currentChord[i]))
+                    chordComplexity += 1.5;
+            }
+
+            return chordComplexity + specialColumn;
         }
     }
 }
