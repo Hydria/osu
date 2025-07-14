035b8945b57529be5405d5d63d37050b8ab82e59
Hydria <liverpol2222@gmail.com>
2025-07-14 16:03:09 +0100

    LN Fix + Chord Fix + Roll pre-fix setup
    Publishing for now so this work isn't lost, will fix this up again when i have the energy

--- osu.Game.Rulesets.Mania/Difficulty/Skills/Strain.cs
+++ osu.Game.Rulesets.Mania/Difficulty/Skills/Strain.cs
@@ -8,6 +8,7 @@ using osu.Game.Rulesets.Difficulty.Skills;
 using osu.Game.Rulesets.Difficulty.Utils;
 using osu.Game.Rulesets.Mania.Difficulty.Preprocessing;
 using osu.Game.Rulesets.Mods;
+using osuTK.Graphics.OpenGL;
 
 namespace osu.Game.Rulesets.Mania.Difficulty.Skills
 {
@@ -24,6 +25,14 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
         private readonly double[] endTimes;
         private readonly double[] individualStrains;
 
+        private double[] noteTimingDifference;
+        private double[] noteTimingSorter;
+        private double[] noteTimingOrderer;
+        private double midpoint = 0;
+
+        private double chordCount = 0;
+        private double previousChordCount = 0;
+
         private double individualStrain;
         private double overallStrain;
 
@@ -33,7 +42,11 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
             startTimes = new double[totalColumns];
             endTimes = new double[totalColumns];
             individualStrains = new double[totalColumns];
+            noteTimingDifference = new double[totalColumns];
+            noteTimingSorter = new double[totalColumns];
+            noteTimingOrderer = new double[totalColumns];
             overallStrain = 1;
+            midpoint = Math.Ceiling(totalColumns / 2.0d);
         }
 
         protected override double StrainValueOf(DifficultyHitObject current)
@@ -59,8 +72,10 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
                 if (Precision.DefinitelyBigger(endTimes[i], endTime, 1) &&
                     Precision.DefinitelyBigger(startTime, startTimes[i], 1))
                     holdFactor = 1.25;
-
-                closestEndTime = Math.Min(closestEndTime, Math.Abs(endTime - endTimes[i]));
+                if (!(Math.Abs(startTime - endTimes[i]) <= 1))
+                {
+                    closestEndTime = Math.Min(Math.Min(closestEndTime, Math.Abs(endTime - endTimes[i])), Math.Abs(startTime - endTimes[i]));
+                }
             }
 
             // The hold addition is given if there was an overlap, however it is only valid if there are no other note with a similar ending.
@@ -83,9 +98,59 @@ namespace osu.Game.Rulesets.Mania.Difficulty.Skills
             // For notes at the same time (in a chord), the individualStrain should be the hardest individualStrain out of those columns
             individualStrain = maniaCurrent.DeltaTime <= 1 ? Math.Max(individualStrain, individualStrains[column]) : individualStrains[column];
 
+            /* ROLL FACTOR IDEA:
+             * WE LOOK AT EACH PRIOR NOTE AND:
+             * 1. WE JUDGE THE PREVIOUS NOTE IN THE SAME COLUMN BASED ON HOW FAR AWAY IT WAS IN COMPARISON TO THE OTHER NOTES
+             * 2. WE JUDGE HOW MANY HAND SWITCHES HAVE TO BE MADE IN ORDER TO PLAY ALL THE PREVIOUS PATTERNS, SO [1234] IS ONE HAND SWITCH, [1324] IS THREE.
+             * NOTE: What do we want as a cutoff, 400ms?
+             */
+
+            //work out the ms difference between the current note at the previous note in each column
+            for (int i = 0; i < noteTimingDifference.Length; i++) noteTimingDifference[i] = startTime - startTimes[i];
+            //in a new array, put them in order
+            noteTimingDifference.CopyTo(noteTimingSorter, 0);
+            Array.Sort(noteTimingSorter);
+            //compare each value in 1st array and designate where it shows up (first instance) in 2nd array
+            for (int j = 0; j < noteTimingOrderer.Length; j++)
+            {
+                if (noteTimingDifference[j] > 400) noteTimingOrderer[j] = 400;
+                else noteTimingOrderer[j] = Array.IndexOf(noteTimingSorter, noteTimingDifference[j]); //TODO: make sure the gap between notes isn't too small, ? 20ms?
+            }
+            //work out the midpoint of the column count (2 for 4K, 3.5 for 7K for example)
+            //for examples where it's .5, exclude the next number up as it is a spacebar
+            // --- UPDATE --- that's now done at the top
+
+            //view where each number is in the array [0,1,2] and count how many times it switches between the midpoint (max column count) (exclude any above 400ms)
+            int currentNote = column;
+            bool currentProgressOngoing = true;
+            int currentProgress = 0;
+            int handSwitches = 0;
+
+            if (noteTimingOrderer[column] > Math.Ceiling(noteTimingDifference.Length / 2.0d)) //we need to limit this check to only the amount of notes featured in the 400ms gap otherwise it causes keymode parity issues
+            {
+                while (currentProgressOngoing)
+                {
+                    int arrayIndex = Array.IndexOf(noteTimingOrderer, currentProgress); //TODO: this needs to consider chords
+                    if (arrayIndex == -1) break;
+                    if (currentNote > midpoint) { if (arrayIndex <= midpoint) handSwitches++; }
+                    if (currentNote <= midpoint) { if (arrayIndex > midpoint) handSwitches++; }
+                    currentProgress++;
+                    currentNote = arrayIndex;
+                }
+            }
+            double rollFactor = Math.Pow(0.75, (currentProgress - handSwitches));
+
+            
+            if (maniaCurrent.DeltaTime <= 1)
+            { chordCount += 1; }
+            else
+            {
+                previousChordCount = chordCount;
+                chordCount = 0;
+            }
             // Decay and increase overallStrain
-            overallStrain = applyDecay(overallStrain, current.DeltaTime, overall_decay_base);
-            overallStrain += (1 + holdAddition) * holdFactor;
+            overallStrain = applyDecay(overallStrain, current.DeltaTime, overall_decay_base) * (Math.Pow(0.997, chordCount + previousChordCount));
+            overallStrain += (1 + holdAddition) * holdFactor * rollFactor;
 
             // Update startTimes and endTimes arrays
             startTimes[column] = startTime;
